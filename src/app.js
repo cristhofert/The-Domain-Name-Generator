35dec255fc38090aface8df9ee979ada888056dd
cristhofert <cristhofert97@gmail.com>
2021-04-21 12:48:13 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let tld = [".us", ".com", ".net"];
+
+  let domains = "";
+  for (const p in pronoun) {
+    for (const a in adj) {
+      for (const n in noun) {
+        for (const d in tld) {
+          domains += pronoun[p] + adj[a] + noun[n] + tld[d] + "<br>";
+        }
+      }
+    }
+  }
+  document.getElementById("domains").innerHTML = domains;
+  this.console.log(domains);
 };
